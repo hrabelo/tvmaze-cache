b56b5c80e0aa3374bbf78690b0b96f7353ba155a
Hian Castilho <hian_castilho@poli.ufrj.br>
2022-11-29 20:35:16 +0100

    First commit

---
+++ src/TVMazeCache.WebApi/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddConsole();
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseStaticFiles();
+
+app.MapControllers();
+
+app.Run();
---
+++ src/TVMazeCache.WebApi/V1/Controllers/ShowsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TVMazeCache.WebApi.V1.Controllers
+{
+    [Route("shows")]
+    [ApiController]
+    public class ShowsController : ControllerBase
+    {
+        public ShowsController() { }
+
+        [HttpGet]
+        public async Task Get()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
