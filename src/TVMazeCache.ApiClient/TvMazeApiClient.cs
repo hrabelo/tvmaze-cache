26ecc46e3b3c12a1bfd0183849a22aa4ab81b4d3
Hian Castilho <hian_castilho@poli.ufrj.br>
2022-12-01 17:42:28 +0100

    Implement TvMazeApiClient

---
+++ src/TVMazeCache.ApiClient/TvMazeApiClient.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using TVMazeCache.ApiClient.Models;
+using TVMazeCache.Domain.Models;
+using TVMazeCache.Domain.Ports;
+
+namespace TVMazeCache.ApiClient
+{
+    public class TvMazeApiClient : ITvMazeApiClient
+    {
+        private static string _getShowsEndpoint(int page) => $"shows?page={page}";
+        private static string _getCastEndpoint(int showId) => $"shows/{showId}/cast";
+
+        private readonly Func<HttpClient> _httpClientFactory;
+
+        public TvMazeApiClient(Func<HttpClient> httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IEnumerable<Show>> GetShows(int page, CancellationToken token)
+        {
+            var httpClient = _httpClientFactory();
+            var response = await httpClient.GetAsync(_getShowsEndpoint(page), token);
+
+            var resultAsString = await response.Content.ReadAsStringAsync();
+            var showsDto = JsonConvert.DeserializeObject<IEnumerable<ShowDto>>(resultAsString);
+            return showsDto!.Select(_ => _.ToDomain());
+        }
+
+        public async Task<IEnumerable<Person>> GetCast(int showId, CancellationToken token)
+        {
+            var httpClient = _httpClientFactory();
+            var response = await httpClient.GetAsync(_getCastEndpoint(showId), token);
+
+            var resultAsString = await response.Content.ReadAsStringAsync();
+            var castDto = JsonConvert.DeserializeObject<IEnumerable<CastDto>>(resultAsString);
+            return castDto!.Select(_ => _.Person.ToDomain());
+        }
+
+    }
+}
