9a77380c50254491335e49b8dde91c0b9a4c52c5
Hian Castilho <hian_castilho@poli.ufrj.br>
2022-12-01 18:10:53 +0100

    Add minimal use case for retrieving shows with cast

---
+++ src/TVMazeCache.Domain/UseCases/RetrieveShowsWithCastUseCase.cs
@@ -0,0 +1,16 @@
+﻿using TVMazeCache.Domain.Models;
+
+namespace TVMazeCache.Domain.UseCases
+{
+    public class RetrieveShowsWithCastUseCase
+    {
+        public RetrieveShowsWithCastUseCase()
+        {
+        }
+
+        public async Task<IEnumerable<Show>> Execute(int page)
+        {
+            return await Task.FromResult(new List<Show>());
+        }
+    }
+}
