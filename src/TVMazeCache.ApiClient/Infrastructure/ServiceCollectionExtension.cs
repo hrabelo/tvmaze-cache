060192dbe9e9ef10f40204233a26ff291099ec6a
Hian Castilho <hian_castilho@poli.ufrj.br>
2022-12-02 00:52:43 +0100

    Refactor policy handling for polling

--- src/TVMazeCache.ApiClient/Infrastructure/ServiceCollectionExtension.cs
+++ src/TVMazeCache.ApiClient/Infrastructure/ServiceCollectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Polly;
 using Polly.Extensions.Http;
 using TVMazeCache.Domain.Ports;
@@ -9,7 +10,8 @@ namespace TVMazeCache.ApiClient.Infrastructure
     {
         public static void AddTvMazeApiClient(
             this IServiceCollection services, 
-            string clientName, TvMazeApiClientSettings settings)
+            string clientName, 
+            TvMazeApiClientSettings settings)
         {
             services.AddHttpClient(
                 name: clientName,
@@ -18,18 +20,38 @@ namespace TVMazeCache.ApiClient.Infrastructure
                     client.BaseAddress = new Uri(settings.BaseUrl!);
                     client.Timeout = TimeSpan.FromMilliseconds(settings.TimeoutMilliseconds);
                 })
-                .AddPolicyHandler(GetRetryPolicy(settings));
+                .AddPolicyHandler((services, request) => GetRetryPolicyForRateLimit(settings, services.GetRequiredService<ILogger<TvMazeApiClient>>()))
+                .AddPolicyHandler((services, request) => GetRetryPolicyForTransientErrors(services.GetRequiredService<ILogger<TvMazeApiClient>>()));
+                
 
             services.AddTransient<ITvMazeApiClient>(sp =>
                 new TvMazeApiClient(() => sp.GetRequiredService<IHttpClientFactory>().CreateClient(clientName)));
         }
 
-        static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy(TvMazeApiClientSettings settings)
+        static IAsyncPolicy<HttpResponseMessage> GetRetryPolicyForRateLimit(TvMazeApiClientSettings settings, ILogger<TvMazeApiClient> logger)
+        {
+            return Policy
+                .HandleResult<HttpResponseMessage>(msg => msg.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+                .WaitAndRetryAsync(
+                    settings.RetryCount,
+                    retryAttepmt => TimeSpan.FromSeconds(settings.WaitIntervalSeconds),
+                    onRetry: (outcome, timespan, retryAttempt, context) =>
+                    {
+                        logger.LogWarning("Too many requests when processing path {Path}. Delaying for {Delay} ms, then making retry {Retry}.", outcome.Result.RequestMessage!.RequestUri!.AbsolutePath, timespan.TotalMilliseconds, retryAttempt);
+                    });
+        }
+
+        static IAsyncPolicy<HttpResponseMessage> GetRetryPolicyForTransientErrors(ILogger<TvMazeApiClient> logger)
         {
             return HttpPolicyExtensions
                 .HandleTransientHttpError()
-                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
-                .WaitAndRetryAsync(settings.RetryCount, retryAttempt => TimeSpan.FromSeconds(settings.WaitIntervalSeconds));
+                .WaitAndRetryAsync(
+                    retryCount: 3,
+                    retryAttepmt => TimeSpan.FromSeconds(5),
+                    onRetry: (outcome, timespan, retryAttempt, context) =>
+                    {
+                        logger.LogWarning("{ReasonPhrase} when processing path {Path}. Delaying for {Delay} ms, then making retry {Retry}.", outcome.Result.ReasonPhrase, outcome.Result.RequestMessage!.RequestUri!.AbsolutePath, timespan.TotalMilliseconds, retryAttempt);
+                    });
         }
     }
 }
