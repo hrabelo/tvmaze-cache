f81861063174978fbdc5e59caa76d3aa48f90250
Hian Castilho <hian_castilho@poli.ufrj.br>
2022-12-01 18:33:56 +0100

    Implement minimum required for test to pass

--- src/TVMazeCache.Domain/UseCases/StoreShowsWithCastUseCase.cs
+++ src/TVMazeCache.Domain/UseCases/StoreShowsWithCastUseCase.cs
@@ -1,18 +1,29 @@
-﻿using Microsoft.Extensions.Logging;
-using TVMazeCache.Domain.Models;
+﻿using TVMazeCache.Domain.Models;
 using TVMazeCache.Domain.Ports;
 
 namespace TVMazeCache.Domain.UseCases
 {
     public class StoreShowsWithCastUseCase
     {
-        public StoreShowsWithCastUseCase()
+        private readonly ITvMazeApiClient _tvMazeApiClient;
+        private readonly IShowsRepository _showsRepository;
+
+        public StoreShowsWithCastUseCase(
+            ITvMazeApiClient tvMazeApiClient,
+            IShowsRepository showsRepository)
         {
+            _tvMazeApiClient = tvMazeApiClient;
+            _showsRepository = showsRepository;
         }
 
         public async Task<IngestedBatchResult> Execute(int page, CancellationToken cancellationToken)
         {
-            return await Task.FromResult(IngestedBatchResult.Success);
+            var shows = await _tvMazeApiClient.GetShows(page, cancellationToken);
+
+            if (!shows.Any())
+                return IngestedBatchResult.NothingToProcess;
+
+            return IngestedBatchResult.Success;
         }
     }
 }
