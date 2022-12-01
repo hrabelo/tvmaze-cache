0ccceeaebea41546950a655a6b65dbecc7f401c1
Hian Castilho <hian_castilho@poli.ufrj.br>
2022-12-01 18:31:48 +0100

    Fail test because of lack of implementation

---
+++ tests/TVMazeCache.Domain.Tests.Unit/UseCases/StoreShowsWithCastUseCaseTests.cs
@@ -0,0 +1,42 @@
+﻿using FluentAssertions;
+using Moq;
+using TVMazeCache.Domain.Models;
+using TVMazeCache.Domain.Ports;
+using TVMazeCache.Domain.UseCases;
+using Xunit;
+
+namespace TVMazeCache.Domain.Tests.Unit.UseCases
+{
+    public class StoreShowsWithCastUseCaseTests
+    {
+
+        private readonly Mock<ITvMazeApiClient> _tvMazeApiClientMock;
+        private readonly Mock<IShowsRepository> _showsRepositoryMock;
+
+        public StoreShowsWithCastUseCaseTests()
+        {
+            _tvMazeApiClientMock = new Mock<ITvMazeApiClient>(MockBehavior.Strict);
+            _showsRepositoryMock = new Mock<IShowsRepository>(MockBehavior.Strict);
+        }
+
+        [Fact]
+        public async Task Execute_WhenShowsListIsEmpty_ShouldReturnIngestedBatchResultNothingToProcess()
+        {
+            var page = 0;
+            var cts = new CancellationToken();
+
+            TvMazeApiClientReturnsEmptyShowList();
+
+            var result = await CreateSut().Execute(page, cts);
+
+            result.Should().Be(IngestedBatchResult.NothingToProcess);
+        }
+
+        private void TvMazeApiClientReturnsEmptyShowList() =>
+            _tvMazeApiClientMock.Setup(_ => _.GetShows(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(Enumerable.Empty<Show>());
+
+        private StoreShowsWithCastUseCase CreateSut() =>
+            new(_tvMazeApiClientMock.Object, _showsRepositoryMock.Object);
+    }
+}
