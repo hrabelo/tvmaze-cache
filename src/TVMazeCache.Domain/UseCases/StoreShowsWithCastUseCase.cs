90a30361d82bd7711904e77d118d7a69b34765ac
Hian Castilho <hian_castilho@poli.ufrj.br>
2022-12-01 18:10:41 +0100

    Add minimal use case for storing shows with cast

---
+++ src/TVMazeCache.Domain/UseCases/StoreShowsWithCastUseCase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Logging;
+using TVMazeCache.Domain.Models;
+using TVMazeCache.Domain.Ports;
+
+namespace TVMazeCache.Domain.UseCases
+{
+    public class StoreShowsWithCastUseCase
+    {
+        public StoreShowsWithCastUseCase()
+        {
+        }
+
+        public async Task<IngestedBatchResult> Execute(int page, CancellationToken cancellationToken)
+        {
+            return await Task.FromResult(IngestedBatchResult.Success);
+        }
+    }
+}
